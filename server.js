dc383225ce319000580e798160f4e8eaf720fa94
MD RAKIB <rakibislamdev@gmail.com>
2023-11-11 01:16:24 +0600

    dotenv added

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
@@ -7,7 +8,7 @@ const routes = require('./routes/routes');
 const app = express();
 
 // Database
-mongoose.connect('mongodb://localhost:27017/user-tracking-api', {
+mongoose.connect(process.env.DB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
@@ -29,7 +30,7 @@ app.use(bodyParser.json());
 
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server listening at port ${PORT}`);
